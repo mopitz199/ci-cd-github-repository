ced61f8ab865a077a73d9a7fa0794d015fdf6b2c
Maximiliano Opitz Turra <mopitz199@gmail.com>
2026-04-02 22:33:27 -0300

    .

--- user/models.py
+++ user/models.py
@@ -6,8 +6,8 @@ from mysite.settings import get_private_storage, get_public_storage
 class UserProfile(models.Model):
 	name = models.CharField(max_length=255)
 	age = models.PositiveIntegerField()
-	profile_image = models.ImageField(upload_to="profiles/", storage=get_public_storage())
-	identity_document = models.ImageField(upload_to="identity/", storage=get_private_storage())
+	profile_image = models.ImageField(upload_to="profiles/", storage=get_public_storage(), null=True)
+	identity_document = models.ImageField(upload_to="identity/", storage=get_private_storage(), null=True)
 
 	def __str__(self):
 		return self.name
